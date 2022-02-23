78aec6c36c921378b58f48b20f2012ca27f0d1c7
nagaraju90108 <sivanagarajuodugu@gmail.com>
2022-02-23 16:11:27 +0530

    Todo List created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       newItem: "",
+       list: []
+    }
+  }
+
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    });
+  }
+  
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    const list = [...this.state.list];
+    list.push(newItem);
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({list: updatedList});
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        Add an Item: 
+        <br/>
+        <input
+          type='text' 
+          placeholder = 'Type item here...'
+          value={this.state.newItem}
+          onChange = {e => this.updateInput('newItem', e.target.value)}
+        />
+        <button onClick={() => this.addItem()}>Add</button>
+        <br />
+        <ol>
+          {this.state.list.map(item => {
+            return(
+              <li key = {item.id}>
+                {item.value}
+                <button onClick={() => this.deleteItem(item.id)} >
+                  X
+                </button>
+              </li>
+            )
+          })
+          }
+        </ol>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
