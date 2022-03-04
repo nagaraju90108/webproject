bec0f86ff111f3d97ec2c0b3af3ff6f56c1e8862
nagaraju90108 <sivanagarajuodugu@gmail.com>
2022-03-04 14:13:55 +0530

    updated for local storage

--- src/App.js
+++ src/App.js
@@ -1,91 +1,103 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Todoform from "./Components/Todoform";
 // import './App.css';
 export class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       newItem: "",
-       list: []
-    }
-  }
+  // constructor(props) {
+  //   super(props);
 
-  updateInput(key, value){
-    this.setState({
-      [key]: value
-    });
-  }
-  
-  addItem() {
-    const newItem = {
-      id: 1 + Math.random(),
-      value: this.state.newItem.slice()
-    };
+  //   this.state = {
+  //     newItem: "",
+  //     list: [],
+  //   };
+  // }
 
-    const list = [...this.state.list];
-    list.push(newItem);
-    this.setState({
-      list,
-      newItem:""
-    });
-  }
+  // updateInput(key, value) {
+  //   this.setState({
+  //     [key]: value,
+  //   });
+  // }
 
-  deleteItem(id) {
-    const list = [...this.state.list];
-    const updatedList = list.filter(item => item.id !== id);
-    this.setState({list: updatedList});
-  }
+  // addItem() {
+  //   const newItem = {
+  //     id: 1 + Math.random(),
+  //     value: this.state.newItem.slice(),
+  //   };
 
-  enterPressed(event) {
-    var code = event.keyCode || event.which;
-    if(code === 13) { 
-      document.getElementById("mybutton").click();
-    } 
-}
+  //   const list = [...this.state.list];
+  //   list.push(newItem);
+  //   this.setState({
+  //     list,
+  //     newItem: "",
+  //   });
+  // }
+
+  // deleteItem(id) {
+  //   const list = [...this.state.list];
+  //   const updatedList = list.filter((item) => item.id !== id);
+  //   this.setState({ list: updatedList });
+  // }
+
+  // enterPressed(event) {
+  //   var code = event.keyCode || event.which;
+  //   if (code === 13) {
+  //     document.getElementById("mybutton").click();
+  //   }
+  // }
 
   render() {
     return (
-      <div className='App flex h-screen bg-rose-50'>
-        <div class="flex-1">
-          <div className='todolist py-2 font-serif text-4xl bg-rose-500 text-center text-white sm:text-7xl sm:py-4'>ToDo List</div>
-          <div className='content'>
-            <div className='additem text-center mt-6 -mb-4 text-xl font-semibold text-rose-600 sm:text-3xl lg:text-4xl lg:-mb-2'>
-              Add an Item: 
-            </div>
-            <br/>
-            <div className='myinput text-center'>
-              <input
-                id='takeinput'
-                type='text' 
-                placeholder = 'Type item here...'
-                value={this.state.newItem}
-                onChange = {e => this.updateInput('newItem', e.target.value)}
-                onKeyPress={this.enterPressed.bind(this)}
-                className = 'border-[1px] border-rose-400 rounded-md pl-2 p-1'
-              />
-              <button id='mybutton' onClick={() => this.addItem()} className= 'ml-4 px-4 py-[2px] border-[1px] border-rose-600 rounded-md text-rose-600 text-lg font-semibold bg-white hover:bg-rose-500 hover:text-white'>ADD</button>
-              <br />
-              <div className=' pt-4 text-lg text-red-500 m-1/6 '>
-              <ol>
-                {this.state.list.map(item => {
-                  return(
-                    <li key = {item.id}>
-                      {item.value}
-                      <button onClick={() => this.deleteItem(item.id)} className= 'px-2 m-2 border-[1px] border-rose-400 rounded-md text-sm text-rose-600 font-bold bg-white hover:bg-rose-500 hover:text-white' >
-                        X
-                      </button>
-                    </li>
-                  )
-                })
-                }
-              </ol>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+      // <div className="App flex h-screen bg-rose-50">
+      //   <div className="flex-1">
+      //     <div className="todolist py-2 font-serif text-4xl bg-rose-500 text-center text-white sm:text-7xl sm:py-4">
+      //       ToDo List
+      //     </div>
+      //     <div className="content">
+      //       <div className="additem text-center mt-6 -mb-4 text-xl font-semibold text-rose-600 sm:text-3xl lg:text-4xl lg:-mb-2">
+      //         Add an Item:
+      //       </div>
+      //       <br />
+      //       <div className="myinput text-center">
+      //         <input
+      //           id="takeinput"
+      //           type="text"
+      //           placeholder="Type item here..."
+      //           value={this.state.newItem}
+      //           onChange={(e) => this.updateInput("newItem", e.target.value)}
+      //           onKeyPress={this.enterPressed.bind(this)}
+      //           className="border-[1px] border-rose-400 rounded-md pl-2 p-1 focus:outline-yellow-400 "
+      //         />
+      //         <button
+      //           id="mybutton"
+      //           onClick={() => this.addItem()}
+      //           className="ml-4 px-4 py-[2px] border-[1px] border-rose-600 rounded-md text-rose-600 text-lg font-semibold bg-white hover:bg-rose-500 hover:text-white"
+      //         >
+      //           ADD
+      //         </button>
+      //         <br />
+      //         <div className=" pt-4 text-lg text-red-500 m-1/6 ">
+      //           <ol>
+      //             {this.state.list.map((item) => {
+      //               return (
+      //                 <li key={item.id}>
+      //                   {item.value}
+      //                   <button
+      //                     onClick={() => this.deleteItem(item.id)}
+      //                     className="px-2 m-2 border-[1px] border-rose-400 rounded-md text-sm text-rose-600 font-bold bg-white hover:bg-rose-500 hover:text-white"
+      //                   >
+      //                     X
+      //                   </button>
+      //                 </li>
+      //               );
+      //             })}
+      //           </ol>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
+      <Todoform />
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/Components/Todoform.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from "react";
+import deleteIcon from "./trashIcon.png";
+const getDatafromLS = () => {
+  const data = localStorage.getItem("NamesList");
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return [];
+  }
+};
+
+export const Todoform = () => {
+  const [namesList, setNamesList] = useState(getDatafromLS());
+
+  const [myid, setMyid] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let lis = {
+      myid: 1 + Math.random(),
+      name,
+    };
+    setNamesList([...namesList, lis]);
+    setMyid("");
+    setName("");
+  };
+
+  const deleteName = (myid) => {
+    const filteredNames = namesList.filter((element, index) => {
+      return element.myid !== myid;
+    });
+    setNamesList(filteredNames);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("NamesList", JSON.stringify(namesList));
+  }, [namesList]);
+
+  return (
+    <div className="App flex h-screen bg-rose-50">
+      <div className="flex-1 text-center">
+        <div className="tracking-widest uppercase todolist py-2 font-serif text-4xl bg-rose-500 text-center text-white sm:text-5xl sm:py-4">
+          ToDo List
+        </div>
+        <div className="additem text-center mt-6 -mb-4 text-xl font-semibold text-rose-600 sm:text-2xl lg:text-3xl lg:-mb-2">
+          Add an Item:
+        </div>
+        <form
+          autoComplete="off"
+          onSubmit={handleSubmit}
+          className="text-center"
+        >
+          <br />
+          <input
+            type="text"
+            placeholder="Type item here..."
+            className="sm:text-2xl lg:text-3xl sm:p-2 sm:pl-4 border-[1px] border-rose-400 rounded-md pl-2 p-1 focus:outline-yellow-400 "
+            onChange={(e) => setName(e.target.value)}
+            required
+            value={name}
+          ></input>
+          <button
+            type="submit"
+            className="sm:text-2xl sm:py-[8px] lg:text-3xl lg:py-[9px]  ml-4 px-6 py-[2px] border-[1px] border-rose-600 rounded-md text-rose-600 text-lg font-semibold bg-white hover:bg-rose-500 hover:text-white"
+          >
+            ADD
+          </button>
+        </form>
+
+        <div className="view-container flex flex-col items-center">
+          {namesList.length > 0 && (
+            <>
+              <div className="flex flex-row justify-center">
+                <table className="mt-8 sm:mt-16 border border-rose-600">
+                  <thead className="border-b border-rose-300 font-semibold text-rose-600 text-xl lg:-mb-2">
+                    <tr>
+                      <th className="mx-4 my-2 px-4 py-3 uppercase">Delete</th>
+                      <th className="mx-4 my-2 px-4 py-3 text-left uppercase">
+                        Name
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {namesList.map((nameHere) => (
+                      <tr
+                        key={nameHere.myid}
+                        className="border-b border-rose-300 lg:text-lg"
+                      >
+                        <td>
+                          <button
+                            onClick={() => deleteName(nameHere.myid)}
+                            className="px-2 text-sm text-rose-600 font-bold bg-transparent"
+                          >
+                            <img
+                              src={deleteIcon}
+                              alt="myIMG"
+                              className="h-5 hover:h-6"
+                            />
+                          </button>
+                        </td>
+                        <td className="mx-4 my-2 px-6 py-2 text-left capitalize">
+                          {nameHere.name}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <button
+                onClick={() => setNamesList([])}
+                className="mt-4 px-4 py-[2px] border-[1px] border-rose-600 rounded-md text-rose-600 text-lg font-semibold bg-white hover:bg-rose-500 hover:text-white"
+              >
+                REMOVE ALL
+              </button>
+            </>
+          )}
+          {namesList.length < 1 && (
+            <div className="bg-rose-200 w-[300px] sm:w-[420px] sm:py-2 lg:w-[495px] lg:text-xl mt-8 sm:mt-14 text-lg py-1 text-center ">
+              No task is added yet
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todoform;
